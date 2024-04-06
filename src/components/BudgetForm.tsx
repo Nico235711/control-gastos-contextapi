526818ac6134ecb0d70b63b60e8b5ba0c24ba247
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-04-06 19:59:51 -0300

    Llamando las acciones del reducer desde el Hook por medio del Context

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,10 @@
-import { ChangeEvent, useMemo, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 const BudgetForm = () => {
 
   const [budget, setBudget] = useState(0)
+  const { state, dispatch } = useBudget()
 
   const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
     setBudget(+e.target.value)
@@ -11,8 +13,13 @@ const BudgetForm = () => {
   // validando
   const isValid = useMemo(() => isNaN(budget) || budget <= 0, [budget])
 
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch({ type: "add-budget", payload: { budget } })
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <div className="flex flex-col space-y-5">
         <label htmlFor="budget" className="text-4xl text-blue-800 font-bold text-center">Definir Presupuesto</label>
         <input
--- src/hooks/useBudget.ts
+++ src/hooks/useBudget.ts
@@ -4,6 +4,7 @@ import { BudgetContext } from "../context/BudgetContext";
 export const useBudget = () => {
   const context = useContext(BudgetContext)
   if (!context) {
+    // tira un error si no hay un context
     throw new Error("useContext must be used within a BudgetProvider");
   }
   return context
