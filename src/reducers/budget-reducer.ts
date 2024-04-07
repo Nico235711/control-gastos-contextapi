9fc07b81169a63ef3cecc8225bc7480db2d1bc31
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-04-07 15:09:39 -0300

    Mostrando el Modal cuando agregamos un nuevo gasto

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,23 +1,34 @@
 export type BudgetActions =
-  { type: "add-budget", payload: { budget: number }}
+  { type: "add-budget", payload: { budget: number } } |
+  { type: "show-modal" } 
+
 
 export type BudgetState = {
   budget: number
+  modal: boolean
 }
 
-export const initialState : BudgetState = {
-  budget: 0
+export const initialState: BudgetState = {
+  budget: 0,
+  modal: false
 }
 
 export const budgetReducer = (
-  state : BudgetState = initialState,
-  action : BudgetActions
+  state: BudgetState = initialState,
+  action: BudgetActions
 ) => {
-  
+
   if (action.type === "add-budget") {
     return {
       ...state,
       budget: action.payload.budget
+    }
+  }
+
+  if (action.type === "show-modal") {
+    return {
+      ...state,
+      modal: true
     }
   }
 
