c51281a61c67e163e4e49fad5a84981a2822a3ef
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-04-14 17:21:10 -0300

    Mostrando los iconos de la categoría del gasto

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react"
+import { categories } from "../data/categories"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
@@ -8,17 +10,23 @@ type ExpenseDetailProps = {
 
 const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
 
+  // filtro si ambos id son iguales
+  const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+  console.log(categoryInfo);
+
+
   return (
-    <div className="bg-white shadow-lg p-10 w-full border-b border-b-gray-200 last-of-type:border-none rounded-lg flex justify-between items-center">
-      <div>
-        <div>
-        
-        </div>
-        
-        <div className="space-y-2">
-          <p className="text-slate-600 text-2xl capitalize">{expense.expenseName}</p>
-          <p className="text-slate-600 text-sm capitalize">{formatDate(expense.date!.toString())}</p>
-        </div>
+    <div className="bg-white shadow-lg p-10 w-full border-b border-b-gray-200 last-of-type:border-none rounded-lg flex items-center gap-5">
+      <img
+        src={`/icono_${categoryInfo.icon}.svg`}
+        alt="icono del gasto"
+        className="w-20"
+      />
+
+      <div className="space-y-2 flex-1">
+        <p className="text-slate-600 text-2xl capitalize">{categoryInfo.name}</p>
+        <p className="text-slate-600 text-2xl capitalize">{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm capitalize">{formatDate(expense.date!.toString())}</p>
       </div>
 
       <AmountDisplay amount={expense.amount} />
