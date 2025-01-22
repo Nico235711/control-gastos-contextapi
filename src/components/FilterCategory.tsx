41377f79d3b29437e11ea4815e65bf73f5985f9b
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-22 10:40:06 -0300

    arreglando el comportamiento no deseado

--- src/components/FilterCategory.tsx
+++ src/components/FilterCategory.tsx
@@ -4,7 +4,7 @@ import { useBudget } from "../hooks/useBudget"
 
 const FilterCategory = () => {
 
-  const { dispatch } = useBudget()
+  const { state, dispatch } = useBudget()
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     dispatch({ type: "add-filter-category", payload: { id: e.target.value } })
@@ -15,7 +15,12 @@ const FilterCategory = () => {
       <form>
         <div className="flex flex-col md:flex-row md:items-center gap-5">
           <label htmlFor="category" className="text-3xl text-center font-bold text-gray-600">Filtrar Gastos</label>
-          <select id="category" className="bg-slate-100 p-3 flex-1 rounded-lg" onChange={handleChange}>
+          <select
+            id="category"
+            className="bg-slate-100 p-3 flex-1 rounded-lg"
+            value={state.filteredCategory}
+            onChange={handleChange}
+          >
             <option value="">-- Todas las Categorías --</option>
             {categories.map(category => (
               <option value={category.id} key={category.id}>{category.name}</option>
